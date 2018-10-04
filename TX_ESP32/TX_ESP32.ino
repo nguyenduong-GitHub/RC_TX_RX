f333fbc31e3a096ab39319f21d5bfffcbfb23bb5
duong nguyen <nguyenduongelectronic@gmail.com>
2018-10-04 23:43:13 +0700

    Define Joystick ADC channels

--- TX_ESP32/TX_ESP32.ino
+++ TX_ESP32/TX_ESP32.ino
@@ -1,5 +1,11 @@
 
-const int analogInPin = 2;  // Analog input pin that the potentiometer is attached to
+// define ADC channle for analog sticks 
+const char CH1 = 2;  // Drone Throttle - Joystick left - Up/Down
+const char CH1 = 2;  // Drone Yaw      - Joystick left - Left/Right
+const char CH1 = 2;  // Drone Direction FW/BW        - Joystick right - FW/BW
+const char CH1 = 2;  // Drone Direction Left/Right   - Joystick right - Left/Right  
+const char CH1 = 2;  // Read Internal Battery
+
 void setup() {
   Serial.begin(9600);
 }
