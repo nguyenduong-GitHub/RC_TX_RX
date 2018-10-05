fd56e0d3c9eb3ee6ec16f7e4d1bc14ae560940e4
duong nguyen <nguyenduongelectronic@gmail.com>
2018-10-06 00:09:21 +0700

    Config Channels, mapping

--- TX_ESP32/TX_ESP32.ino
+++ TX_ESP32/TX_ESP32.ino
@@ -5,10 +5,10 @@
 #include "SerialDebug.h"
 #define DEBUG true
 // define ADC channle for analog sticks 
-const char CH0 = 2;  // Drone Throttle - Joystick left - Up/Down
-const char CH1 = 2;  // Drone Yaw      - Joystick left - Left/Right
-const char CH2 = 2;  // Drone Direction FW/BW        - Joystick right - FW/BW
-const char CH3 = 2;  // Drone Direction Left/Right   - Joystick right - Left/Right  
+const char CH0 = 4;  // Drone Throttle - Joystick left - Up/Down
+const char CH1 = 15;  // Drone Yaw      - Joystick left - Left/Right
+const char CH2 = 13;  // Drone Direction FW/BW        - Joystick right - FW/BW
+const char CH3 = 12;  // Drone Direction Left/Right   - Joystick right - Left/Right  
 const char CH4 = 2;  // Read Internal Battery
 
 void setup(){
@@ -21,8 +21,17 @@ void setup(){
 }
 
 void loop(){
-int sensorValue = analogRead(CH0);
-SerialDebugger.print("ADC: ").println(sensorValue);
+int VAL_CH0 = 4095-analogRead(CH0);
+SerialDebugger.print("Throttle_CH0: ").println(VAL_CH0);
+
+int VAL_CH1 = analogRead(CH1);
+SerialDebugger.print("YAW_CH1: ").println(VAL_CH1);
+
+int VAL_CH2 = 4095-analogRead(CH2);
+SerialDebugger.print("DIR_FB_CH2: ").println(VAL_CH2);
+
+int VAL_CH3 = 4095 - analogRead(CH3);
+SerialDebugger.print("DIR_LR_CH3: ").println(VAL_CH3);
   
 delay(1000);
 }
