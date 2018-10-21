4c5b4fe1b76a3b58da3f84b53368f75a7e04ea04
duong nguyen <nguyenduongelectronic@gmail.com>
2018-10-22 00:08:09 +0700

    Use  String payload

--- RX_MEGA328/RX_MEGA328/RX_MEGA328.ino
+++ RX_MEGA328/RX_MEGA328/RX_MEGA328.ino
@@ -6,13 +6,16 @@
 
 
 unsigned char rx_payload[36];
-unsigned int CH0_THR ;  // Drone Throttle - Joystick left - Up/Down
-unsigned int CH1_YAW ;  // Drone Yaw      - Joystick left - Left/Right
-unsigned int CH2_FB ;  // Drone Direction FW/BW        - Joystick right - FW/BW
-unsigned int CH3_LR ;  // Drone Direction Left/Right   - Joystick right - Left/Right  
+int CH0_THR ;  // Drone Throttle - Joystick left - Up/Down
+int CH1_YAW ;  // Drone Yaw      - Joystick left - Left/Right
+int CH2_FB ;  // Drone Direction FW/BW        - Joystick right - FW/BW
+int CH3_LR ;  // Drone Direction Left/Right   - Joystick right - Left/Right  
 unsigned char i = 0;
-unsigned char rx_char;
- 
+char rx_char_new ;
+char rx_char_old ;
+String VAL_CH0 = "";
+String VAL_CH2 = ""; 
+String VAL_CH3 = ""; 
 SoftwareSerial mySerial(10, 11); // RX, TX
 
 void setup()
@@ -25,12 +28,12 @@ void setup()
   digitalWrite(M_IN2, LOW);
   digitalWrite(M_IN3, LOW);
   digitalWrite(M_IN4, HIGH);
-  delay(1000);
+  delay(500);
   digitalWrite(M_IN1, LOW);
   digitalWrite(M_IN2, HIGH);
   digitalWrite(M_IN3, HIGH);
   digitalWrite(M_IN4, LOW);
-  delay(1000);
+  delay(500);
   digitalWrite(M_IN1, LOW);
   digitalWrite(M_IN2, LOW);
   digitalWrite(M_IN3, LOW);
@@ -52,6 +55,42 @@ void car_motor_intial()
   digitalWrite(M_IN2, LOW);
   digitalWrite(M_IN3, LOW);
   digitalWrite(M_IN4, LOW);
+}
+void wheel_left(char dir)
+{
+  if(dir == 'U')
+  {
+    digitalWrite(M_IN1, HIGH);
+    digitalWrite(M_IN2, LOW);
+  }
+  if(dir == 'D')
+  {
+    digitalWrite(M_IN1, LOW);
+    digitalWrite(M_IN2, HIGH);
+  }
+   if(dir == 'P')
+  {
+    digitalWrite(M_IN1, LOW);
+    digitalWrite(M_IN2, LOW);
+  }
+}
+void wheel_right(char dir)
+{
+  if(dir == 'U')
+  {
+    digitalWrite(M_IN3, LOW);
+    digitalWrite(M_IN4, HIGH);
+  }
+  if(dir == 'D')
+  {
+    digitalWrite(M_IN3, HIGH);
+    digitalWrite(M_IN4, LOW);
+  }
+   if(dir == 'P')
+  {
+    digitalWrite(M_IN3, LOW);
+    digitalWrite(M_IN4, LOW);
+  }
 }
 void car_move_up()
 {
@@ -91,37 +130,94 @@ void car_stop()
 void loop() // run over and over
 {
   if (mySerial.available())
-    { rx_char = mySerial.read();
-      if(rx_char == 0xFF)
-      { i = 0;}
-      else
-      {
-        i++;
-        rx_payload[i]= rx_char;
-      }
-      if(i>30)
-      {  i= 0;
-        //parser rx_payload 
-        //Serial.write(rx_payload[1]);
-        //Serial.write(rx_payload[2]);
-        CH0_THR =  rx_payload[2]; CH0_THR=CH0_THR<<8;  CH0_THR = CH0_THR|rx_payload[1]; CH0_THR = CH0_THR & 0x1FFF ;
-        CH1_YAW =  rx_payload[4]; CH1_YAW=CH1_YAW<<8;  CH1_YAW = CH1_YAW|rx_payload[3]; CH1_YAW = CH1_YAW & 0xFFF ;
-        CH2_FB =  rx_payload[6]; CH2_FB=CH2_FB<<8;  CH2_FB = CH2_FB|rx_payload[5]; CH2_FB = CH2_FB & 0xFFF ;
-        CH3_LR =  rx_payload[8]; CH3_LR=CH3_LR<<8;  CH3_LR = CH3_LR|rx_payload[7]; CH3_LR = CH3_LR & 0xFFF ;
-       //for(int j=1; j<=2; j++){Serial.write(rx_payload[j]);i= 0;}
-        Serial.print("CH0_THR: ");Serial.println(CH0_THR); // Center = 2288; Up = 4623, down = 0
-        Serial.print("CH1_YAW: "); Serial.println(CH1_YAW); //center = 1915, Left = 0, Right = 3848
-        Serial.print("CH2_FB: "); Serial.println(CH2_FB);   //center = 2204 
-        Serial.print("CH3_LR: "); Serial.println(CH3_LR);   //center =2222
-        if(CH0_THR >3000){car_move_up();}
-        if(CH0_THR <500){car_move_down();}
-        if(CH1_YAW <500){car_turn_left();}
-        if(CH1_YAW >3700){car_turn_right();}
-        if((CH0_THR >500)&&(CH0_THR <3000)&&(CH1_YAW >500)&&(CH1_YAW<3700)){car_stop();}
-      
-    }
+    { rx_char_new = mySerial.read();
+      if(rx_char_new == 'A') 
+         { 
+           while(rx_char_new !=';')
+           {
+            if (mySerial.available())
+              {
+                rx_char_new = mySerial.read();
+                if(rx_char_new != ';')
+                {
+                 VAL_CH0 = VAL_CH0 + rx_char_new;
+                }
+              }
+           }
+           Serial.println(VAL_CH0);
+           CH0_THR = VAL_CH0.toInt();
+           Serial.println(CH0_THR);
+           VAL_CH0 = "";
+          
+         }
+       if(rx_char_new == 'C') 
+         { 
+           while(rx_char_new !=';')
+           {
+            if (mySerial.available())
+              {
+                rx_char_new =  mySerial.read();
+                if(rx_char_new != ';')
+                  {
+                  VAL_CH2 = VAL_CH2 + rx_char_new ;
+                  }
+                  //Serial.println(rx_char_new);
+             
+              }
+           }
+           Serial.println(VAL_CH2);
+           CH2_FB = VAL_CH2.toInt();
+           Serial.println(CH2_FB);
+           VAL_CH2 = "";
+           
+         }
+         if(rx_char_new == 'D') 
+         { 
+           while(rx_char_new !=';')
+           {
+            if (mySerial.available())
+              {
+                rx_char_new =  mySerial.read();
+                if(rx_char_new != ';')
+                  {
+                  VAL_CH3 = VAL_CH3 + rx_char_new ;
+                  }
+                  //Serial.println(rx_char_new);
+             
+              }
+           }
+           Serial.println(VAL_CH3);
+           CH3_LR = VAL_CH3.toInt();
+           Serial.println(CH3_LR);
+           VAL_CH3 = "";
+           
+         }
+           //CH0_THR =  rx_payload[1]; CH0_THR=CH0_THR<<8;  CH0_THR = CH0_THR|rx_payload[0]; CH0_THR = CH0_THR & 0x1FFF ;
+          // CH1_YAW =  rx_payload[3]; CH1_YAW=CH1_YAW<<8;  CH1_YAW = CH1_YAW|rx_payload[2]; CH1_YAW = CH1_YAW & 0xFFF ;
+          // CH2_FB =  rx_payload[5]; CH2_FB=CH2_FB<<8;  CH2_FB = CH2_FB|rx_payload[4]; CH2_FB = CH2_FB & 0xFFF ;
+           //CH3_LR =  rx_payload[7]; CH3_LR=CH3_LR<<8;  CH3_LR = CH3_LR|rx_payload[6]; CH3_LR = CH3_LR & 0xFFF ;
+          //for(int j=1; j<=2; j++){Serial.write(rx_payload[j]);i= 0;}
+          //Serial.print("CH0_THR: ");  Serial.println(CH0_THR); // Center = 2288; Up = 4623, down = 0
+         // Serial.print("CH1_YAW: ");  Serial.println(CH1_YAW); //center = 1915, Left = 0, Right = 3848
+         // Serial.print("CH2_FB: ");   Serial.println(CH2_FB);   //center = 2204 
+         // Serial.print("CH3_LR: ");   Serial.println(CH3_LR);   //center =2222
+         // if(CH0_THR >3000){wheel_left('U'); }
+         //  if(CH0_THR <500) {wheel_left('D'); }
+         // if(CH2_FB >3000) {wheel_right('U');}
+         // if(CH2_FB <500)  {wheel_right('D');}
+            if(CH2_FB <500){car_move_up();}
+            if(CH2_FB >3700){car_move_down();}
+            if(CH3_LR <500){car_turn_left();}
+            if(CH3_LR >3700){car_turn_right();}
+          //if((CH0_THR >500)&&(CH0_THR <3000)){wheel_left('P');}  
+          if((CH2_FB >500)&&(CH2_FB <3000) && (CH3_LR >500)&&(CH3_LR <3700)){car_stop();}
+             
+     
+     
+     
     
     }  
     
 }
+
 
--- TX_ESP32/TX_ESP32.ino
+++ TX_ESP32/TX_ESP32.ino
@@ -11,9 +11,7 @@ const char CH1 = 15;  // Drone Yaw      - Joystick left - Left/Right
 const char CH2 = 13;  // Drone Direction FW/BW        - Joystick right - FW/BW
 const char CH3 = 12;  // Drone Direction Left/Right   - Joystick right - Left/Right  
 const char CH4 = 2;  // Read Internal Battery
-
-unsigned int payload[18]; // 1 byte start, 8 bytes of Channles, 8 bytes of functions, 1 byte stop
-unsigned char payload_leng;
+String new_payload = "";
 void setup(){
  /*if (DEBUG){
     //SerialDebugger.begin(2,9600); //can select uart
@@ -27,23 +25,21 @@ void setup(){
 
 void loop(){
  // Serial2.write('a');
-unsigned int VAL_CH0 = 4095-analogRead(CH0); payload[1] = VAL_CH0;
+unsigned int VAL_CH0 = 4095-analogRead(CH0); 
+if (VAL_CH0>4000){VAL_CH0 = 4000;}
+//Serial.print("Throttle_CH0: "); Serial.print(VAL_CH0);
 
-//SerialDebugger.print("Throttle_CH0: ").println(VAL_CH0);
+unsigned int VAL_CH1 = analogRead(CH1);
 
-unsigned int VAL_CH1 = analogRead(CH1);payload[2] = VAL_CH1;
-//SerialDebugger.print("YAW_CH1: ").println(VAL_CH1);
+unsigned int VAL_CH2 = 4095-analogRead(CH2);
+if (VAL_CH2>4000){VAL_CH2 = 4000;}
+unsigned int VAL_CH3 = 4095 - analogRead(CH3);
 
-unsigned int VAL_CH2 = 4095-analogRead(CH2);payload[3] = VAL_CH2;
-//SerialDebugger.print("DIR_FB_CH2: ").println(VAL_CH2);
+new_payload = 'A' + String(VAL_CH0, DEC)+ ';' + 'B' +String(VAL_CH1, DEC) + ';' + 'C' +String(VAL_CH2, DEC) + ';' + 'D' +String(VAL_CH3, DEC) + ';'; 
+Serial.println(new_payload);
+Serial2.println(new_payload);
 
-unsigned int VAL_CH3 = 4095 - analogRead(CH3);payload[4] = VAL_CH3;
-//SerialDebugger.print("DIR_LR_CH3: ").println(VAL_CH3);
-
-payload[0]= 0xFFFF;
-payload[18]= 0xEEEE;;
-
-for(int i=0; i<=18; i++){Serial2.write(payload[i]);Serial2.write(payload[i]>>8);} // Send payload to uart
-for(int i=0; i<=18; i++){Serial.write(payload[i]);Serial.write(payload[i]>>8);}
+//for(int i=0; i<=18; i++){Serial2.write(payload[i]);Serial2.write(payload[i]>>8);} // Send payload to uart
+//for(int i=0; i<=18; i++){Serial.write(payload[i]);Serial.write(payload[i]>>8);}
 delay(100);
 }
